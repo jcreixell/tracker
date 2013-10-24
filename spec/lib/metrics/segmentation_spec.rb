1411edd2d46cb7240ea9beb1f1b282287468d719
jcreixell <jcreixell@gmail.com>
2013-10-24 15:59:35 +0600

    Improve test readability

--- spec/lib/metrics/segmentation_spec.rb
+++ spec/lib/metrics/segmentation_spec.rb
@@ -6,18 +6,20 @@ module Metrics
       let(:project) { FactoryGirl.create(:project) }
 
       it "filters events by date" do
-        event_dates = [
+        included_dates = [
           DateTime.new(2013, 1, 5),
           DateTime.new(2013, 12, 2),
+        ]
+        excluded_dates = [
           DateTime.new(2012, 1, 2),
           DateTime.new(2015, 4, 2)
         ]
-        event_dates.each { |date| project.events.create(type: "sale", time: date, properties: { amount: 10 }) }
+        (included_dates + excluded_dates).each { |date| project.events.create(type: "sale", time: date, properties: { amount: 10 }) }
 
         result = Metrics::Segmentation.new(project, "sale", DateTime.new(2013, 1, 1), DateTime.new(2014, 1, 1)).process
         result_dates = result.map{ |elem| elem.keys.first }
-        event_dates[0..1].each { |date| result_dates.should include date }
-        event_dates[2..3].each { |date| result_dates.should_not include date }
+        included_dates.each { |date| result_dates.should include date }
+        excluded_dates.each { |date| result_dates.should_not include date }
       end
 
     end
